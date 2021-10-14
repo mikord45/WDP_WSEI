31e847483d528b6dfdae975cb928f304c30b1b91
mikord45 <51155742+mikord45@users.noreply.github.com>
2021-10-14 20:54:22 +0200

    lab01-5

---
+++ Lab01/HelloWPFApp/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWPFApp
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxImie_LostFocus(object sender, RoutedEventArgs e)
+        {
+            labelKomunikat1.Content = $"Witaj {textBoxImie.Text} {textBoxNazwisko.Text}!";
+        }
+
+        private void textBoxNazwisko_LostFocus(object sender, RoutedEventArgs e)
+        {
+            labelKomunikat1.Content = $"Witaj {textBoxImie.Text} {textBoxNazwisko.Text}!";
+        }
+
+        private void textBoxWiek_LostFocus(object sender, RoutedEventArgs e)
+        {
+            int wiek = Convert.ToInt32(textBoxWiek.Text);
+            if(wiek < 67)
+            {
+                labelKomunikat2.Content = $"Do emerytury zostało ci {67 - wiek}";
+            }
+            else
+            {
+                labelKomunikat2.Content = "Jesteś emerytem";
+            }
+        }
+
+        private void buttonClear_Click(object sender, RoutedEventArgs e)
+        {
+            textBoxImie.Text = "";
+            textBoxNazwisko.Text = "";
+            textBoxWiek.Text = "";
+            labelKomunikat1.Content = "";
+            labelKomunikat2.Content = "";
+        }
+    }
+}
